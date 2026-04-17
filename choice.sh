2ae0e4459d99c9ced1c5c89957af0920e0feb0b4
Mads Byriel <madshbyriel@gmail.com>
2026-04-17 23:15:38 +0200

    Add sandstorm theme!

--- choice.sh
+++ choice.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 MENU_CMD="${1:-wofi} --dmenu --prompt 'Theme Menu'}"
 
-choice=$(printf "Gruvbox\nCatppuccin\nCyberpunk" | eval "$MENU_CMD")
+choice=$(printf "Gruvbox\nCatppuccin\nCyberpunk\nSandstorm" | eval "$MENU_CMD")
 
 case "$choice" in
     Catppuccin)
@@ -12,5 +12,8 @@ case "$choice" in
         ;;
     Cyberpunk)
         ln -fs ~/.config/hypr/cyberpunk/theme.conf ~/.config/hypr/theme.conf
+        ;;
+    Sandstorm)
+        ln -fs ~/.config/hypr/sandstorm/theme.conf ~/.config/hypr/theme.conf
         ;;
 esac
